6fba94822b27e078a125d74a8d3296843518a8d5
Artem <carbon0093@gmail.com>
2021-09-08 17:27:02 +0300

    HW Fix: добавил несколько модулей и импортировал в один index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import countTimer from './modules/countTimer';
 import toggleMenu from './modules/toggleMenu';
 import popUp from './modules/popUp';
@@ -7,78 +6,30 @@ import addDot from './modules/addDot';
 import slider from './modules/slider';
 import calc from './modules/calc';
 import sendForm from './modules/sendForm';
+import scroll from './modules/scroll';
+import photoAt from './modules/photoAt';
+import calcValid from './modules/calcValid';
 
 //таймер
 // countTimer('23 September  2021');
 
 //меню
 toggleMenu();
-
 //Модальное окно
 popUp();
-
 //скрол
-const menu = document.querySelector('menu'),
-  menuLinks = menu.querySelectorAll('ul>li>a'),
-  btnScroll = document.querySelector('a[href="#service-block"]');
-
-const scrollEvent = event => {
-  event.preventDefault();
-  const itemHash = event.currentTarget.getAttribute('href');
-  document.querySelector('' + itemHash).scrollIntoView({
-    behavior: 'smooth',
-    block: 'start'
-  });
-};
-
-menuLinks.forEach(menuLink => {
-  menuLink.addEventListener('click', scrollEvent);
-});
-
-btnScroll.addEventListener('click', scrollEvent);
-
+scroll();
 //табы
 tabs();
-
 //Script DOT
 addDot();
-
 //слайдер
 slider();
-
 //Атрибуты у фото
-const commandPhoto = document.querySelectorAll('.command__photo');
-
-commandPhoto.forEach((photo, ind) => {
-
-  photo.addEventListener('mouseenter', event => {
-    if (event.target === commandPhoto[ind]) {
-      event.target.dataset.firstImg = commandPhoto[ind].src;
-      commandPhoto[ind].src = event.target.dataset.img;
-    }
-  });
-
-  photo.addEventListener('mouseout', event => {
-    if (event.target === commandPhoto[ind]) {
-      commandPhoto[ind].src = event.target.dataset.firstImg;
-    }
-  });
-
-});
-
-//Регулярки
-const calcBlock = document.querySelector('.calc-block'),
-  inputCalc = calcBlock.querySelectorAll('input');
-
-inputCalc.forEach(item => {
-  const checkValue = () => {
-    item.value = item.value.replace(/[^\d]/g, '');
-  };
-  item.addEventListener('input', checkValue);
-});
-
+photoAt();
+//Валидация калькулятора
+calcValid();
 //Калькулятор
 calc(100);
-
 //ajax работа с сервером
 sendForm();
